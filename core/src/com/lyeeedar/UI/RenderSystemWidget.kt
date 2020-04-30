a8f91025f2b6ae933df3b810201e93ea541c02c2
lyeeedar <lyeeedar@gmail.com>
2020-04-30 08:09:29 +0100

    Add support for attaching widgets to entities.

--- core/src/com/lyeeedar/UI/RenderSystemWidget.kt
+++ core/src/com/lyeeedar/UI/RenderSystemWidget.kt
@@ -6,7 +6,11 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent
 import com.badlogic.gdx.scenes.scene2d.InputListener
 import com.badlogic.gdx.scenes.scene2d.Touchable
 import com.badlogic.gdx.scenes.scene2d.ui.Widget
+import com.badlogic.gdx.utils.Array
+import com.lyeeedar.Components.EntityReference
 import com.lyeeedar.Components.position
+import com.lyeeedar.Components.renderOffset
+import com.lyeeedar.Components.renderable
 import com.lyeeedar.Systems.AbstractTile
 import com.lyeeedar.Systems.World
 import com.lyeeedar.Util.AssetManager
@@ -22,6 +26,8 @@ class RenderSystemWidget(val world: World<*>) : Widget()
 	val selectedPoint: Point
 		get() = screenspaceToPoint(mousePos.x, mousePos.y)
 	var isSelected = false
+
+	var attachedToEntityWidgets = Array<AttachedToEntityWidget>(false, 4)
 
 	init
 	{
@@ -75,7 +81,13 @@ class RenderSystemWidget(val world: World<*>) : Widget()
 
 	fun pointToScreenspace(x: Float, y: Float): Vector2
 	{
-		val playerPos = world.player!!.position()!!.position
+		val playerPos = world.player!!.position()!!.position.toVec()
+		val renderOffset = world.player?.renderable()?.renderable?.animation?.renderOffset(true)
+		if (renderOffset != null)
+		{
+			playerPos.add(renderOffset[0], renderOffset[1])
+		}
+
 		val tileSize = world.tileSize
 
 		val offsetx = Statics.resolution.x * 0.5f - playerPos.x * tileSize - tileSize * 0.5f
@@ -84,8 +96,61 @@ class RenderSystemWidget(val world: World<*>) : Widget()
 		return this.localToStageCoordinates(Vector2(offsetx + x * world.tileSize, offsety + y * world.tileSize))
 	}
 
+	fun addAttachedToEntityWidget(entity: EntityReference, widget: Widget)
+	{
+		val holder = AttachedToEntityWidget(entity, widget)
+		attachedToEntityWidgets.add(holder)
+		update(holder)
+	}
+
+	fun update(attachedToEntityWidget: AttachedToEntityWidget): Boolean
+	{
+		if (attachedToEntityWidget.widget.stage == null) return true
+		val entity = attachedToEntityWidget.entity.get() ?: return true
+
+		val dx = attachedToEntityWidget.widget.x - attachedToEntityWidget.lastPos.x
+		val dy = attachedToEntityWidget.widget.y - attachedToEntityWidget.lastPos.y
+
+		val entityPos = entity.position()!!.position.toVec()
+		val renderOffset = entity.renderable()?.renderable?.animation?.renderOffset(true)
+		if (renderOffset != null)
+		{
+			entityPos.add(renderOffset[0], renderOffset[1])
+		}
+
+		val newPos = pointToScreenspace(entityPos.x, entityPos.y)
+		attachedToEntityWidget.lastPos.set(newPos)
+
+		attachedToEntityWidget.widget.x = newPos.x + dx
+		attachedToEntityWidget.widget.y = newPos.y + dy
+
+		return false
+	}
+
+	override fun act(delta: Float)
+	{
+		val itr = attachedToEntityWidgets.iterator()
+		while (itr.hasNext())
+		{
+			val holder = itr.next()
+			val shouldRemove = update(holder)
+
+			if (shouldRemove)
+			{
+				itr.remove()
+			}
+		}
+
+		super.act(delta)
+	}
+
 	companion object
 	{
 		lateinit var instance: RenderSystemWidget
 	}
+}
+
+class AttachedToEntityWidget(val entity: EntityReference, val widget: Widget)
+{
+	var lastPos: Vector2 = Vector2()
 }
