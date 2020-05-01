04eb016ca75c20aabec249feb6ffe1c69c716468
lyeeedar <lyeeedar@gmail.com>
2020-05-01 13:01:24 +0100

    Allow passing in a blood colour

--- core/src/com/lyeeedar/Util/BloodSplatter.kt
+++ core/src/com/lyeeedar/Util/BloodSplatter.kt
@@ -18,7 +18,7 @@ class BloodSplatter
 
 		val splatters = AssetManager.loadSprite("Oryx/Custom/terrain/bloodsplatter")
 
-		fun splatter(attackSource: Point, bloodSource: Point, emitDist: Float, world: World<*>)
+		fun splatter(attackSource: Point, bloodSource: Point, emitDist: Float, world: World<*>, colour: Colour = Colour.RED)
 		{
 			var vector = bloodSource.toVec()
 			vector.sub(attackSource.x.toFloat(), attackSource.y.toFloat())
@@ -36,7 +36,7 @@ class BloodSplatter
 			sprite.baseScale[0] = 0.3f + Random.random(Random.sharedRandom, 0.3f)
 			sprite.baseScale[1] = sprite.baseScale[0]
 			sprite.rotation = Random.random(Random.sharedRandom, 180f)
-			sprite.colour = Colour.RED.copy().a(0.4f + Random.random(Random.sharedRandom, 0.2f))
+			sprite.colour = colour.copy().a(0.4f + Random.random(Random.sharedRandom, 0.2f))
 
 			val entity = bloodArchetype.build()
 
