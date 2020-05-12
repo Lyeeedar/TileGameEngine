ec9eba6e1667619083f4d162b209e5c0e54d12b0
lyeeedar <lyeeedar@gmail.com>
2020-05-12 15:12:07 +0100

    Fix render system screen to point and point to screen

--- core/src/com/lyeeedar/UI/RenderSystemWidget.kt
+++ core/src/com/lyeeedar/UI/RenderSystemWidget.kt
@@ -5,6 +5,10 @@ import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.scenes.scene2d.InputEvent
 import com.badlogic.gdx.scenes.scene2d.InputListener
 import com.badlogic.gdx.scenes.scene2d.Touchable
+import com.badlogic.gdx.scenes.scene2d.actions.Actions
+import com.badlogic.gdx.scenes.scene2d.actions.Actions.delay
+import com.badlogic.gdx.scenes.scene2d.actions.Actions.removeActor
+import com.badlogic.gdx.scenes.scene2d.ui.Label
 import com.badlogic.gdx.scenes.scene2d.ui.Widget
 import com.badlogic.gdx.utils.Array
 import com.lyeeedar.Components.EntityReference
@@ -17,6 +21,8 @@ import com.lyeeedar.Util.AssetManager
 import com.lyeeedar.Util.Colour
 import com.lyeeedar.Util.Point
 import com.lyeeedar.Util.Statics
+import kotlinx.coroutines.delay
+import ktx.actors.then
 
 class RenderSystemWidget(val world: World<*>) : Widget()
 {
@@ -46,6 +52,21 @@ class RenderSystemWidget(val world: World<*>) : Widget()
 				            return true
 			            }
 
+			            override fun mouseMoved(event: InputEvent?, x: Float, y: Float): Boolean
+			            {
+				            mousePos.set(x, y)
+
+				            super.mouseMoved(event, x, y)
+				            return true
+			            }
+
+			            override fun touchDragged(event: InputEvent?, x: Float, y: Float, pointer: Int)
+			            {
+				            mousePos.set(x, y)
+
+				            super.touchDragged(event, x, y, pointer)
+			            }
+
 			            override fun touchUp(event: InputEvent?, x: Float, y: Float, pointer: Int, button: Int)
 			            {
 				            mousePos.set(x, y)
@@ -57,29 +78,8 @@ class RenderSystemWidget(val world: World<*>) : Widget()
 		            })
 	}
 
-	fun screenspaceToPoint(x: Float, y: Float): Point
-	{
-		val playerPos = world.player!!.position()!!.position
-		val tileSize = world.tileSize
-
-		val offsetx = Statics.resolution.x * 0.5f - playerPos.x * tileSize - tileSize * 0.5f
-		val offsety = Statics.resolution.y * 0.5f - playerPos.y * tileSize - tileSize * 0.5f
-
-		val xp = x - offsetx
-		val yp = y - offsety
-
-		val sx = (xp / world.tileSize).toInt()
-		val sy = (yp / world.tileSize).toInt()
-
-		return Point(sx, sy)
-	}
-
-	fun pointToScreenspace(point: Point): Vector2
-	{
-		return pointToScreenspace(point.xFloat, point.yFloat)
-	}
-
-	fun pointToScreenspace(x: Float, y: Float): Vector2
+	private val offsetVec = Vector2()
+	fun getPlayerOffset(): Vector2
 	{
 		val playerPos = world.player!!.position()!!.position.toVec()
 		val renderOffset = world.player?.renderable()?.renderable?.animation?.renderOffset(true)
@@ -91,9 +91,37 @@ class RenderSystemWidget(val world: World<*>) : Widget()
 		val tileSize = world.tileSize
 
 		val offsetx = Statics.resolution.x * 0.5f - playerPos.x * tileSize - tileSize * 0.5f
-		val offsety = Statics.resolution.y * 0.5f - playerPos.y * tileSize - tileSize * 0.5f
+		val offsety = Statics.resolution.y * 0.5f - playerPos.y * tileSize - tileSize * 2f
 
-		return this.localToStageCoordinates(Vector2(offsetx + x * world.tileSize, offsety + y * world.tileSize))
+		offsetVec.set(offsetx, offsety)
+		return offsetVec
+	}
+
+	fun screenspaceToPoint(x: Float, y: Float): Point
+	{
+		val offset = getPlayerOffset()
+		val tileSize = world.tileSize
+
+		val xp = x - offset.x
+		val yp = y - offset.y
+
+		val sx = (xp / tileSize).toInt()
+		val sy = (yp / tileSize).toInt()
+
+		return Point(sx, sy)
+	}
+
+	fun pointToScreenspace(point: Point): Vector2
+	{
+		return pointToScreenspace(point.xFloat, point.yFloat)
+	}
+
+	fun pointToScreenspace(x: Float, y: Float): Vector2
+	{
+		val offset = getPlayerOffset()
+		val tileSize = world.tileSize
+
+		return this.localToStageCoordinates(Vector2(offset.x + x * tileSize, offset.y + y * tileSize))
 	}
 
 	fun addAttachedToEntityWidget(entity: EntityReference, widget: Widget)
